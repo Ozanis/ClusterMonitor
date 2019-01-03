fcb2e82843732c2ddb61c8113f8c420c801b8c42
Ozanis <37501284+ozanis@users.noreply.github.com>
2019-01-03 23:52:13 +0200

    comit

---
+++ telemetry_server.py
@@ -0,0 +1,35 @@
+import socket, gzip, os, subprocess, ssl
+
+class LocalServ:
+
+    def __init__(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.port = 443
+        self.addr = socket.gethostname()
+        self.buffer = ""
+
+    def beacon_set(self):
+        self.sock.bind((self.addr, self.port))
+        self.sock.listen(1)
+
+    def beacon_recv(self):
+        self.sock.accept()
+
+        with open(self.filename, "wb") as _file:
+            try:
+                while len(self.buffer)<=1024:
+                    self.buffer = self.sock.recv(1024)
+                    _file.write(self.buffer)
+            except:
+                if len(self.buffer) >= 1024:
+                    self.buffer =""
+                else:
+                    self.sock.send("Error of file transfer".encode())
+                    self.sock.close()
+                    self.buffer=""
+
+if __name__ == "__main__":
+    Server = LocalServ()
+    Server.beacon_recv()
+    #receiving files in 1024 bytes packages
+    Server.beacon_recv()
