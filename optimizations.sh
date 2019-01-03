22f889abc3778accc5bc0579faa2d013d06a4287
Ozanis <37501284+Ozanis@users.noreply.github.com>
2019-01-03 19:20:30 +0200

    Create optimizations.sh

---
+++ optimizations.sh
@@ -0,0 +1,47 @@
+! /usr/bin bash
+
+bash outlook.sh
+
+apt purge hexchat
+apt purge thunderbird
+apt purge compiz compiz-plugins compiz-core compiz-gnome compiz-plugins-default compizconfig-settings-manager
+
+apt upgrade
+
+apt install openjdk-8-jdk -y
+
+apt install preload
+
+apt purge laptop-mode-tools
+add-apt-repository ppa:linrunner/tlp
+apt update
+apt install tlp tlp-rdw
+tlp start
+
+echo "
+vm.swapiness =10
+vm.dirty_bytes = 2097152
+vm.dirty_background_bytes = 2097152
+vm.vfs_cache_pressure = 500" >> /etc/sysctl.conf
+
+echo"
+tmpfs /tmp tmpfs defaults,size=2G,mode=1777	0	0" >> /etc/fstab
+
+if [`cat /proc/sys/kernel/sched_autogroup_enabled`];
+then echo >> "\nkernel.sched_autogroup_enabled=1"
+else break
+fi 
+
+systemctl disable openvpn.service
+systemctl disable pppd-dns.service
+systemctl disable ModemManager.service
+systemctl disable speech-dispatcher.service
+systemctl disable cgmanager.service
+systemctl disable NetworkManager-wait-online.service 
+
+apt autoremove
+apt autoclean
+
+systemd-analyze
+
+exit 0
