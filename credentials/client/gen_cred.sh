3a76fb5660768f8db90d9cbd0c03e391f9234c5e
Ozanis <37501284+ozanis@users.noreply.github.com>
2019-01-13 23:23:17 +0200

    Commit

--- credentials/client/gen_cred.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-openssl genrsa -out key.pem 2048
-openssl req -new -key key.pem -out csr.pem -sha512
-openssl req -nodes -newkey rsa:2048 -keyout key.pem -out csr.pem  -sha512
-openssl req -new -x509  -days 365 -key key.pem  -out crt.pem -sha512
-
-openssl x509 -noout -modulus -in crt.pem | openssl md5
-openssl x509 -noout -modulus -in key.pem | openssl md5
-openssl x509 -noout -modulus -in csr.pem | openssl md5
