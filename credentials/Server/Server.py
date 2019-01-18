07b3f968a6bd4f2b19cac7df9e29949dc41aa1b8
Ozanis <37501284+ozanis@users.noreply.github.com>
2019-01-18 21:58:35 +0200

    Commit

--- credentials/Server/Server.py
+++ credentials/Server/Server.py
@@ -10,34 +10,34 @@ class SockSsl:
             subprocess.Popen(['notify-send', "Сould not create socket"])
             logging.error("Socket creation error")
             exit(1)
+        self.context = None
         try:
-            self.context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+            self.context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
         except ssl.SSLEOFError:
             subprocess.Popen(['notify-send', "SSL context loading failed"])
             logging.error("Fake context")
             exit(1)
         try:
-            path = str(os.getcwd()) + "/credentials/Server/"
-            self.context.check_hostname = True
-            self.context.verify_mode = ssl.CERT_REQUIRED
-            self.context.load_cert_chain(certfile=path + "crt.pem", keyfile=path + "key.pem")
-            self.context.load_verify_locations(capath=path+"csr.pem")
+            self.context.load_cert_chain(certfile="crt.pem", keyfile="key.pem")
             self.context.options |= ssl.PROTOCOL_TLSv1_2 | ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
             self.context.set_ciphers("TLS13+CDH+AESGCM:ECDH+CHACHA20")
         except ssl.SSLEOFError:
             subprocess.Popen(['notify-send', "Braking cert-chain"])
             logging.error("Corrupted cert-chain")
             exit(1)
+        self.ssl_sock = None
         try:
             self.ssl_sock = self.context.wrap_socket(self.sock, server_side=True, do_handshake_on_connect=True)
+            #self.ssl_sock.s
         except ssl.SSLEOFError:
             subprocess.Popen(['notify-send', "Error of ssl-socket wrapping"])
             logging.error("Error loading cert-chain")
             exit(1)
+        self.host = None
         try:
-            self.port = 4547
+            port = 4547
             self.host = "127.0.0.1"
-            self.ssl_sock.bind((self.host, self.port))
+            self.ssl_sock.bind((self.host, port))
         except OSError:
             subprocess.Popen(['notify-send', "Error of binding"])
             logging.error("Error of binding")
@@ -48,22 +48,27 @@ class SockSsl:
     def con(self):
         self.ssl_sock.listen(1)
         c, p = self.ssl_sock.accept()
-        if c != self.host:
-            try:
-                subprocess.check_call(["ufw", "deny", "incoming", "from", str(c)])
-                logging.warning("Firewall role succefully added: %s (incoming traffic has blocked)" % str(c))
-            except subprocess.CalledProcessError:
-                logging.warning("Firewall role is not added!")
-                exit(1)
-
-    def send(self, data):
-        _buf = gzip.compress(str(data), compresslevel=9)
+        #if c != self.host:
+            #try:
+             #   subprocess.check_call(["ufw", "deny", "incoming", "from", str(c)])
+              #  logging.warning("Firewall role succefully added: %s (incoming traffic has blocked)" % str(c))
+            #except subprocess.CalledProcessError:
+             #   logging.warning("Firewall role is not added!")
+              #  exit(1)
         try:
-            self.ssl_sock.send(_buf)
+            _buf = self.ssl_sock.recv(3072)
+            data = gzip.decompress(_buf)
+            del _buf
+            with open("log/test.log", "wb") as _f:
+                _f.write(data)
         except socket.error:
             subprocess.Popen(['notify-send', "Warning: sending metrics error"])
             exit(1)
 
-    def __del__(self):
-        self.ssl_sock.close()
-        del self.ssl_sock, self.context, self.port, self.host, self.peer, self.peername
+    #def __del__(self):
+        #if self.ssl_sock:
+         #   self.sock.close()
+        #del self.ssl_sock, self.context, self.port, self.host, self.peer, self.peername
+
+c = SockSsl()
+c.con()
--- telemetry_client.py
+++ telemetry_client.py
@@ -4,12 +4,14 @@ import socket, gzip, ssl, os, logging, subprocess
 class SockSsl:
 
     def __init__(self):
+        self.sock = None
         try:
             self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
         except socket.error:
             subprocess.Popen(['notify-send', "Сould not create socket"])
             logging.error("Socket creation error")
             exit(1)
+        self.context = None
         try:
             self.context = ssl.SSLContext(ssl.PROTOCOL_TLS)
         except ssl.SSLEOFError:
@@ -18,12 +20,12 @@ class SockSsl:
             exit(1)
         path = str(os.getcwd()) + "/credentials/client/"
         try:
-            self.context.check_hostname = True
             self.context.verify_mode = ssl.CERT_REQUIRED
             #self.context.load_cert_chain(certfile=path + "crt.pem", keyfile=path + "key.pem")
             self.context.load_verify_locations(capath=path + "crt.pem")
             #self.context.options |= ssl.PROTOCOL_TLSv1_2 | ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1
             self.context.options = ssl.PROTOCOL_TLSv1_2
+            self.context.check_hostname = True
             #self.context.set_ciphers("TLS13+CDH+AESGCM:ECDH+CHACHA20")
         except ssl.SSLEOFError:
             subprocess.Popen(['notify-send', "Braking cert-chain"])
@@ -35,8 +37,9 @@ class SockSsl:
             subprocess.Popen(['notify-send', "Error of ssl-socket wrapping"])
             logging.error("Error loading cert-chain")
             exit(1)
+        del path
 
-    def con(self, data):
+    def con(self, val):
         try:
             port = 4547
             host = "127.0.0.1"
@@ -45,14 +48,33 @@ class SockSsl:
             subprocess.Popen(['notify-send', "Error of connection"])
             logging.error("Error of binding")
             exit(1)
-        _buf = gzip.compress(str(data), compresslevel=9)
+        buf = gzip.compress(val, compresslevel=9)
         try:
-            self.ssl_sock.send(_buf.encode())
+            self.ssl_sock.send(buf)
         except socket.error:
             subprocess.Popen(['notify-send', "Warning: sending metrics error"])
             exit(1)
 
     def __del__(self):
-        self.ssl_sock.close()
-        self.sock.close()
-        del self.ssl_sock, self.context, self.sock
+        if self.ssl_sock is not None:
+            del self.context
+            self.ssl_sock.unwrap()
+            self.ssl_sock.close()
+            del self.ssl_sock
+            self.sock.close()
+            del self.sock
+        else:
+            if self.sock is not None:
+                del self.sock
+                if self.context is not None:
+                    del self.context
+                    if self.ssl_sock is not None:
+                        del self.ssl_sock
+
+c = SockSsl()
+with open("file.txt", "wb") as _f:
+    _f.write(b"dffffffffffff")
+with open("file.txt", "rb") as _f:
+    _buf = _f.read()
+    c.con(_buf)
+os.remove("file.txt")
